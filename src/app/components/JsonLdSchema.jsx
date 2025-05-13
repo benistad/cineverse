aa92de598d1d2b71a98935c30e7907900dee9485
Cascade AI <cascade@example.com>
2025-05-13 11:31:14 +0200

    Correction du balisage structuré pour l'affichage du logo dans les résultats Google

--- src/app/components/JsonLdSchema.jsx
+++ src/app/components/JsonLdSchema.jsx
@@ -1,53 +1,58 @@
-'use client';
-
-import { useEffect } from 'react';
+// Suppression de 'use client' pour permettre le rendu côté serveur
+import Script from 'next/script';
 
 /**
  * Composant pour ajouter le balisage structuré JSON-LD à la page
  * Cela aide Google à comprendre le site et à afficher le logo dans les résultats de recherche
  */
-export default function JsonLdSchema() {
-  useEffect(() => {
-    // Le script est ajouté côté client pour éviter les problèmes avec SSR
-  }, []);
-
-  // Données structurées pour le site web (Schema.org)
-  const websiteSchema = {
-    '@context': 'https://schema.org',
-    '@type': 'WebSite',
-    'name': 'MovieHunt',
-    'url': 'https://www.moviehunt.fr',
-    'potentialAction': {
-      '@type': 'SearchAction',
-      'target': 'https://www.moviehunt.fr/search?q={search_term_string}',
-      'query-input': 'required name=search_term_string'
+// Données structurées pour le site web et l'organisation (Schema.org)
+const schemaData = {
+  '@context': 'https://schema.org',
+  '@graph': [
+    {
+      '@type': 'WebSite',
+      '@id': 'https://www.moviehunt.fr/#website',
+      'url': 'https://www.moviehunt.fr',
+      'name': 'MovieHunt',
+      'description': 'Trouvez votre prochain film coup de cœur',
+      'potentialAction': [
+        {
+          '@type': 'SearchAction',
+          'target': 'https://www.moviehunt.fr/search?q={search_term_string}',
+          'query-input': 'required name=search_term_string'
+        }
+      ],
+      'inLanguage': 'fr-FR'
+    },
+    {
+      '@type': 'Organization',
+      '@id': 'https://www.moviehunt.fr/#organization',
+      'name': 'MovieHunt',
+      'url': 'https://www.moviehunt.fr',
+      'logo': {
+        '@type': 'ImageObject',
+        '@id': 'https://www.moviehunt.fr/#logo',
+        'inLanguage': 'fr-FR',
+        'url': 'https://www.moviehunt.fr/favicon.png',
+        'contentUrl': 'https://www.moviehunt.fr/favicon.png',
+        'width': 192,
+        'height': 192,
+        'caption': 'MovieHunt'
+      },
+      'image': {
+        '@id': 'https://www.moviehunt.fr/#logo'
+      }
     }
-  };
+  ]
+};
 
-  // Données structurées pour l'organisation (Schema.org)
-  const organizationSchema = {
-    '@context': 'https://schema.org',
-    '@type': 'Organization',
-    'name': 'MovieHunt',
-    'url': 'https://www.moviehunt.fr',
-    'logo': 'https://www.moviehunt.fr/favicon.png',
-    'sameAs': [
-      // Ajoutez ici vos liens sociaux si vous en avez
-      // 'https://twitter.com/moviehunt',
-      // 'https://facebook.com/moviehunt',
-    ]
-  };
-
+export default function JsonLdSchema() {
   return (
-    <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
-      />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
-      />
-    </>
+    <Script
+      id="schema-org"
+      type="application/ld+json"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+    />
   );
 }
